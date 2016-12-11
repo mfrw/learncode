6c77cf92e422dbad2ae594f50d4868eb6525f9d5
Muhammad Falak R Wani <falakreyaz@gmail.com>
2016-12-11 22:48:02 +0530

    graph: add bfs function

--- graph/traversal.cpp
+++ graph/traversal.cpp
@@ -13,6 +13,7 @@ class graph {
 	void add_edge(int src, int dest);
 	void add_edge(int src, int dest, bool dag);
 	void dfs(int src);
+	void bfs(int src);
 };
 
 graph::graph(int v)
@@ -49,16 +50,47 @@ void graph::dfs(int src)
 	dfsutil(src, visited);
 }
 
+void graph::bfs(int src)
+{
+	bool *visited = new bool[verts];
+	for(int i = 0; i < verts; i++)
+		visited[i] = false;
+	list<int> queue;
+
+	visited[src] = true;
+	queue.push_back(src);
+
+	list<int>::iterator i;
+	while(!queue.empty()) {
+		src = queue.front();
+		cout << src << " ";
+		queue.pop_front();
+		for(i = alist[src].begin(); i != alist[src].end(); i++) {
+			if(!visited[*i]) {
+				visited[*i] = true;
+				queue.push_back(*i);
+			}
+		}
+	}
+}
+
+
 
 int main() {
-	graph g(4);
+	graph g(6);
 	g.add_edge(0, 1, false);
 	g.add_edge(0, 2, false);
 	g.add_edge(1, 2, false);
 	g.add_edge(2, 3, false);
+	g.add_edge(3, 4, false);
+	g.add_edge(4, 5, false);
+	g.add_edge(5, 0, false);
 
 	cout << "following is dfs \n";
-	g.dfs(2);
+	g.dfs(0);
+	cout << endl;
+	cout << "following is bfs \n";
+	g.bfs(0);
 	cout << endl;
 	return 0;
 }
